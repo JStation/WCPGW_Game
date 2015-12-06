62dfd32e32bd29398fdab572191c6253a49f0eb7
Matt <matt.roberts@asinglesolution.com>
2015-12-05 21:31:10 -0800

    Simple Game class, load_json_objects function, from_json Asset class method

--- src/game.py
+++ src/game.py
@@ -1,13 +1,40 @@
 import cmd
+import os
+import json
+from asset import Asset
 from goon import Goon
+from player import Player
 
 
-assets = []
+class Game(object):
+    PATH_ASSETS = 'data/assets/'
 
-def get_asset(asset_id):
-    for asset in assets:
-        if asset_id == asset.asset_id:
-            return asset
+    def __init__(self):
+        self._assets = []
+        self._player = Player()
+
+        self.load_assets()
+
+    def load_assets(self):
+        for asset in Game.load_json_objects(self.PATH_ASSETS):
+            print asset
+            self._assets.append(Asset.from_json(asset))
+
+    def get_asset(self, asset_id):
+        for asset in self._assets:
+            if asset_id == asset.asset_id:
+                return asset
+
+    @staticmethod
+    def load_json_objects(json_path):
+        objects = []
+        json_files = [json_file for json_file in os.listdir(json_path) if json_file.endswith('.json')]
+        for json_file in json_files:
+            with open(os.path.join(json_path, json_file)) as json_file_object:
+                objects.append(json.load(json_file_object))
+        return objects
+
+game = Game()
 
 
 class game_cmd(cmd.Cmd, object):
