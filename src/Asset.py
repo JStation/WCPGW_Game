77d28e6bc400fee9ce3a20d8d10908d407f35ec5
Matt <matt.roberts@asinglesolution.com>
2015-12-05 20:53:42 -0800

    .gitignore, lower case file names...

--- src/Asset.py
+++ src/Asset.py
@@ -4,4 +4,8 @@ class Asset(object):
     Assets that a player can purchase, like vehicles, weapons or mission requirement items like "Mega Torch 3000" to
     cut through vault doors!
     """
-    pass
+
+    def __init__(self, asset_id, name, description):
+        self._asset_id = asset_id
+        self._name = name
+        self._description = description
